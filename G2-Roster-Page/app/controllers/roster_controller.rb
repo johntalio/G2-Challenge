b4a9027cf0b5261c238bf929ecaf0019de1a1227
John Talio <johntalio@yahoo.com>
2017-04-14 18:20:03 -0400

    Must figure out how to increment vote count

--- G2-Roster-Page/app/controllers/roster_controller.rb
+++ G2-Roster-Page/app/controllers/roster_controller.rb
@@ -3,4 +3,9 @@ class RosterController < ApplicationController
   def roster_call
     @users = User.all
   end
+
+  def create
+    user = User.find(params[:id])
+    User.vote_count += 1
+  end
 end
