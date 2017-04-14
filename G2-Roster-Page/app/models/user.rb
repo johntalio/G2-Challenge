8a1125a76f92eb08734538b6a08bbbaffec8895a
John Talio <johntalio@yahoo.com>
2017-04-14 16:09:54 -0400

    Requesting API data through user model

--- G2-Roster-Page/app/models/user.rb
+++ G2-Roster-Page/app/models/user.rb
@@ -1,15 +1,6 @@
 class User < ApplicationRecord
-  # Thought about using one of these two methods to access api data, but will probably
-  # use HTTParty instead...
-
-  # From Stack Overflow
-  # recent_posts = HTTParty.get "http://www.example.org/?json=get_recent_posts"
-  # puts recent_posts.status, recent_posts.count
-  #
-  # recent_posts.posts.each do |post|
-  #   puts post.title #guessing
-  # end
-
-  # From Truthy
-  # @opened_uri[:roster] = JSON.parse(open(roster, 'Accept-Encoding' => '') {|f| f.read})
+  roster = HTTParty.get "https://api.myjson.com/bins/jhz5z"
+  roster.each do |r|
+    self.create!(id: r["id"], image_url: r["image_url"], name: r["name"], title: r["title"], bio: r["bio"])
+  end
 end
