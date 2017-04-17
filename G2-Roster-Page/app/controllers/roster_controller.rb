468c15701a449a6cdd706d985f08a03cfa4e33d2
John Talio <johntalio@yahoo.com>
2017-04-17 12:43:26 -0400

    After much work on controller, xhr request is sending data

--- G2-Roster-Page/app/controllers/roster_controller.rb
+++ G2-Roster-Page/app/controllers/roster_controller.rb
@@ -5,7 +5,14 @@ class RosterController < ApplicationController
   end
 
   def create
-    user = User.find(params[:id])
-    User.vote_count += 1
+    @user = User.find(params[:user_id])
+    @user.vote_count += 1
+    if request.xhr?
+      respond_to do |format|
+        format.html { render :layout => false }
+      end
+    else
+      flash[:danger] = "Could not cast vote."
+    end
   end
 end
