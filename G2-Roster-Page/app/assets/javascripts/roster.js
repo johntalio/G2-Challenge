02a7fe5ddf988479af15354c48e9951c8629730b
John Talio <johntalio@yahoo.com>
2017-04-18 12:20:59 -0400

    jQuery is unnecessary - just using AJAX

--- G2-Roster-Page/app/assets/javascripts/roster.js
+++ G2-Roster-Page/app/assets/javascripts/roster.js
@@ -1,17 +1,18 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
-$(document).ready(function() {
-  $(".roster-container").on("click", ".button_to", function(event) {
-    event.preventDefault();
-    var $form = $(this);
-    var method = $form.attr("method");
-    var url = $form.attr("action");
-    $.ajax({
-      method: method,
-      url: url
-    }).done(function(response){
-      console.log("hi", response)
-      $form.parent().find("user.vote_count").html(response)
-    })
-  });
-});
+// $(document).ready(function() {
+//   $(".roster-container").on("click", ".button_to", function(event) {
+//     event.preventDefault();
+//     var $form = $(this);
+//     var method = $form.attr("method");
+//     var url = $form.attr("action");
+//     $.ajax({
+//       method: method,
+//       url: url,
+//       dataType: 'jsonp'
+//     }).done(function(response){
+//       console.log("hi", response)
+//       $form.parent().find(".vote-count").html(response.vote_count)
+//     })
+//   });
+// });
